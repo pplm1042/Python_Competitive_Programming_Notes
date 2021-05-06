1a67ff142f44281011d018efe54314af1c378f16
pplm1042 <pplm1042@naver.com>
2021-05-06 18:18:28 +0900

    sys_tip

---
+++ sys_tip.py
@@ -0,0 +1,10 @@
+################################################
+# DFS 사용 시 recursion 에러를 방지하기 위한 세팅
+import sys
+
+sys.setrecursionlimit(1000)
+
+# 입력 속도를 빠르게 하기 위한 세팅
+import sys
+
+sys.stdin.readline
